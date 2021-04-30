7c563390ebba4161944bd8409b8f1d388569c934
molnattila991 <molnarattila991@gmail.com>
2021-04-30 12:21:25 +0200

    -(edit) tdd part 4

--- lib/features/number_trivia/domain/entities/number_trivia.dart
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -1,10 +1,12 @@
 import 'package:equatable/equatable.dart';
-
 import 'package:meta/meta.dart';
 
-class NumberTrivia {
+class NumberTrivia extends Equatable {
   final String text;
   final int number;
 
-  NumberTrivia({@required this.text, @required this.number});
+  NumberTrivia({@required this.text, @required this.number}) : super();
+
+  @override
+  List<Object> get props => [text, number];
 }
